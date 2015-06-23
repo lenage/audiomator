94afebc6f05810d3720a0f0341d17d464494363f
Yuan He <lendage@gmail.com>
2015-06-23 15:04:12 +0800

    Add metadata for audio

--- lib/audiomator/options.rb
+++ lib/audiomator/options.rb
@@ -1,12 +1,19 @@
+require 'shellwords'
 module Audiomator
   # Options which convert audio to
-  class Options < Struct.new(:start_time, :end_time, :bitrate)
-    def initialize(start_time, end_time, bitrate = '32k')
+  class Options < Struct.new(:start_time, :end_time, :bitrate, :metadata)
+    def initialize(start_time, end_time, bitrate = '32k', metadata = {})
       super
     end
 
+    # see http://multimedia.cx/eggs/supplying-ffmpeg-with-metadata/ for metadata
+    def metadata_string
+      return '' if metadata.empty?
+      metadata.map { |k, v| "-metadata #{k}=#{Shellwords.escape(v)}" }.join(' ')
+    end
+
     def to_s
-      "-b:a #{bitrate} -ss #{start_time} -to #{end_time}"
+      "-b:a #{bitrate} -ss #{start_time} -to #{end_time} #{metadata_string}"
     end
   end
 end
--- spec/audiomator/options_spec.rb
+++ spec/audiomator/options_spec.rb
@@ -10,4 +10,12 @@ describe Audiomator::Options do
     options = Audiomator::Options.new('0:00:06.98', '0:00:08.48')
     expect(options.bitrate).to eql '32k'
   end
+
+  it 'should be able set metadata' do
+    metadata = { title: 'This is Tina' }
+    options = Audiomator::Options.new('0:00:06.98', '0:00:08.48', nil, metadata)
+    expect(options.metadata).to eql metadata
+    expect(options.metadata_string).to be_kind_of String
+    expect(options.metadata_string).to include "Tina"
+  end
 end
